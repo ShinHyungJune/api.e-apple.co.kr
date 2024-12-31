c60087af8bc04e812153b95eb3192f4ca045e6c7
obpark <oesoft52@gmail.com>
2024-12-31 10:35:36 +0900

    cron

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Console\Commands\ExpirePoints;
 use App\Console\Commands\TestTodayDataSeed;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
@@ -11,16 +10,18 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 
-Schedule::command(ExpirePoints::class)
-    ->withoutOverlapping()
-    //->everySecond();
-    ->daily()//자정에 실행
-;
-
-
+// 개발에서만 실행
 Schedule::command(TestTodayDataSeed::class)
     ->withoutOverlapping()
-    //->dailyAt('09:53')
     ->daily()//자정에 실행
     //->environments(['local'])
 ;
+
+
+/*Schedule::command(ExpirePoints::class)
+    ->withoutOverlapping()
+    //->everySecond();
+    ->daily()//자정에 실행
+;*/
+
+
