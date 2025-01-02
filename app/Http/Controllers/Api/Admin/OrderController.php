1c06dc4a9b0aec5bce1014a2c47632436b5b0e04
obpark <oesoft52@gmail.com>
2025-01-02 15:08:11 +0900

    테스트용 임시

--- app/Http/Controllers/Api/Admin/OrderController.php
+++ app/Http/Controllers/Api/Admin/OrderController.php
@@ -3,22 +3,35 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Api\ApiController;
+use App\Models\Order;
 use App\Models\Product;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class OrderController extends ApiController
 {
     /**
      * 테스트용 임시
      */
-    public function test()
+    public function test(Request $request)
     {
-        $user = User::findOrFail(2);
-        $user->update(['email' => 'test@test.com', 'password' => '123456', 'name' => '테스트', 'phone' => '01091767659']);
+        if ($request->input('setup')) {
+            $user = User::findOrFail(2);
+            $user->update(['email' => 'test@test.com', 'password' => '123456', 'name' => '테스트', 'phone' => '01091767659']);
 
-        $product = Product::findOrFail(2);
-        $product->update(['price' => 1000, 'delivery_fee' => 0]);
-        $product->options()->update(['price' => 500, 'stock_quantity' => 1000]);
+            $product = Product::findOrFail(2);
+            $product->update(['price' => 1000, 'delivery_fee' => 0]);
+            $product->options()->update(['price' => 500, 'stock_quantity' => 1000]);
+        }
+
+        if (
+            $request->input('id') && $request->input('status')
+        ) {
+            Order::findOrFail($request->input('id'))
+                ->update(['status' => $request->input('status')]);
+
+        }
+
 
         return $this->respondSuccessfully($user);
     }
