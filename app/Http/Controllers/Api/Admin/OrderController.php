59ffb7fba8fd79e19a5f3c278c035a3517270b19
obpark <oesoft52@gmail.com>
2025-01-02 11:26:28 +0900

    테스트용 임시

--- app/Http/Controllers/Api/Admin/OrderController.php
+++ app/Http/Controllers/Api/Admin/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Api\ApiController;
+use App\Models\Product;
 use App\Models\User;
 
 class OrderController extends ApiController
@@ -14,6 +15,11 @@ class OrderController extends ApiController
     {
         $user = User::findOrFail(2);
         $user->update(['email'=>'test@test.com', 'password' => '123456']);
+
+        $product = Product::findOrFail(2);
+        $product->update(['price' => 1000, 'delivery_fee' => 0]);
+        $product->options()->update(['price' => 1000, 'stock_quantity' => 1000]);
+
         return $this->respondSuccessfully($user);
     }
 }
