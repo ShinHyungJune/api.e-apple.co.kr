f0a0dec7885c115f7d34fe3e478b53516e55a5b5
obpark <oesoft52@gmail.com>
2025-01-06 16:55:40 +0900

    product seeder 추가

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,14 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Post\Board;
-use App\Models\Post\BoardCategory;
-use App\Models\Post\Post;
-use App\Models\Product;
-use App\Models\ProductInquiry;
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -20,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::statement("SET foreign_key_checks=0");
+        /*DB::statement("SET foreign_key_checks=0");
         User::truncate();
         Product::truncate();
         BoardCategory::truncate();
@@ -29,14 +22,13 @@ class DatabaseSeeder extends Seeder
         //MdProductPackage::truncate();
         ProductInquiry::truncate();
         DB::table("media")->truncate();
-        DB::statement("SET foreign_key_checks=1");
+        DB::statement("SET foreign_key_checks=1");*/
 
         $this->call([
-            CodeSeeder::class,
+            /*CodeSeeder::class,
             UserSeeder::class,
             BannerSeeder::class,
             SweetnessSeeder::class,
-
 
             BoardSeeder::class,
             BoardCategorySeeder::class,
@@ -46,14 +38,14 @@ class DatabaseSeeder extends Seeder
             //MdProductPackageSeeder::class,
             TestProductDataSeeder::class,
             ProductPackageSeeder::class,
-            //ProductInquirySeeder::class,
+            //ProductInquirySeeder::class,*/
 
             /*CartSeeder::class,
             DeliveryAddressSeeder::class,
             CouponSeeder::class,
-            OrderSeeder::class,
+            OrderSeeder::class,*/
             ProductReviewSeeder::class,
-            PointSeeder::class,
+            /*PointSeeder::class,
             ExchangeReturnSeeder::class,
             InquirySeeder::class,
             TestUserDataSeeder::class,*/
