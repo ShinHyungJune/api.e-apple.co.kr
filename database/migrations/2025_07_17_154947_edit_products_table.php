8f1dbf61f8120b838867120701753677dccf6659
obpark <oesoft52@gmail.com>
2025-07-17 15:52:53 +0900

    제조연원일, 유통기한 date => string

---
+++ database/migrations/2025_07_17_154947_edit_products_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('manufacture_date')->nullable()->comment('제조연원일')->change();
+            $table->string('expiration_date')->nullable()->comment('유통기한')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
