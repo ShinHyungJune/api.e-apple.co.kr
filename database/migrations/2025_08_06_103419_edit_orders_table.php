8a517b070cea7e13c90372f345d6aa785ba52b62
obpark <oesoft52@gmail.com>
2025-08-06 11:44:46 +0900

    가상계좌 => 실시간 계좌이체로 변경

--- database/migrations/2025_08_06_103419_edit_orders_table.php
+++ database/migrations/2025_08_06_103419_edit_orders_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->string("pay_method_method")->nullable()->comment('결제수단 ex) card, vbank, transfer');
+            $table->string("pay_method_method")->nullable()
+                ->comment('결제수단 ex) card, vbank, transfer')
+                ->change();
         });
     }
 
