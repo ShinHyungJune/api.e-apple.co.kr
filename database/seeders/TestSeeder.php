286a18b7ced98b8c48944cd2c87685489d70586d
ShinHyungJune <ssa4141@naver.com>
2025-11-12 17:24:39 +0900

    - seeder 처리

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -20,18 +20,23 @@ class TestSeeder extends Seeder
             User::create([
                 'name' => 'Test User',
                 'email' => 'test@naver.com',
+                'username' => 'test@naver.com',
                 'password' => Hash::make('test@naver.com'),
-                'username' => 'testuser',
             ]);
 
-            $this->command->info('테스트 계정이 생성되었습니다: test@naver.com / test@naver.com');
+            $this->command->info('테스트 계정이 생성되었습니다.');
+            $this->command->info('아이디(username): test@naver.com');
+            $this->command->info('비밀번호: test@naver.com');
         } else {
-            // 이미 존재하면 비밀번호만 업데이트
+            // 이미 존재하면 username과 비밀번호 업데이트
             $user->update([
+                'username' => 'test@naver.com',
                 'password' => Hash::make('test@naver.com'),
             ]);
 
-            $this->command->info('테스트 계정 비밀번호가 업데이트되었습니다: test@naver.com / test@naver.com');
+            $this->command->info('테스트 계정이 업데이트되었습니다.');
+            $this->command->info('아이디(username): test@naver.com');
+            $this->command->info('비밀번호: test@naver.com');
         }
     }
 }
